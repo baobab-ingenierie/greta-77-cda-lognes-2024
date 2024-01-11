85f1db3451b7e91f9d3d2e990ce2d90d7f29173f
baobab-ingenierie <baobab.ingenierie@gmail.com>
2024-01-11 16:44:18 +0100

    JS Day 5

--- foodtruck/assets/js/form_save.js
+++ foodtruck/assets/js/form_save.js
@@ -44,7 +44,8 @@ document.querySelector('#saveCookie').addEventListener(
         if (document.querySelector('#fname').value.trim()) {
             let today = new Date();
             today.setTime(today.getTime() + 7 * 24 * 60 * 60 * 1000);
-            document.cookie = document.querySelector('#fname').value + '=' + getJSON(false) + '; expires=' + today + '; Secure; HttpOnly';
+            document.cookie = document.querySelector('#fname').value + '=' + getJSON(false) + '; expires=' + today.toUTCString() + '; Secure';
+            alert('Sauvegarde Cookie OK !');
         } else {
             alert('Entrer un prénom.')
         }
@@ -62,11 +63,99 @@ document.querySelector('#saveLocal').addEventListener(
             if (localStorage) {
                 localStorage.setItem(key, getJSON(false));
                 sessionStorage.setItem(key, getJSON(false));
+                alert('Sauvegarde Local/Session OK !');
             } else {
                 alert('Stockage local non supporté.');
             }
         } else {
             alert('Il manque le prénom !');
+        }
+    }
+);
+
+/**
+ * Clic sur INDEXDB
+ */
+document.querySelector('#saveIndex').addEventListener(
+    'click',
+    function () {
+        // Teste si IndexDB est supporté
+        if (indexedDB) {
+            // Crée et ouvre une connexion
+            let cnn = indexedDB.open('foodtruck', 1);
+
+            // Crée la DB et les OS
+            cnn.addEventListener(
+                'upgradeneeded',
+                function () {
+                    // Crée la DB
+                    let db = cnn.result;
+
+                    // Crée l'OS si besoin
+                    if (!db.objectStoreNames.contains('candidatures')) {
+                        let os = db.createObjectStore(
+                            'candidatures',
+                            { autoIncrement: true }
+                        );
+                        let idx = os.createIndex(
+                            'idxName',
+                            ['fname']
+                        );
+                    }
+
+                }
+            );
+            
+            // Si connexion OK
+            cnn.addEventListener(
+                'success',
+                function () {
+                    // Ouvre l'OS
+                    let db = cnn.result;
+                    let tx = db.transaction(
+                        ['candidatures'],
+                        'readwrite'
+                    );
+                    let store = tx.objectStore('candidatures');
+
+                    // Stocke les data
+                    let res = store.put(getJSON());
+
+                    // Si stockage OK
+                    res.addEventListener(
+                        'success',
+                        function () {
+                            alert('Sauvegarde IndexDB OK !');
+                        }
+                    );
+
+                    // Si stockage KO
+                    res.addEventListener(
+                        'error',
+                        function (e) {
+                            alert('Stockage KO : ' + e);
+                        }
+                    );
+
+                    // Quand transaction finie
+                    tx.addEventListener(
+                        'complete',
+                        function () {
+                            db.close();
+                        }
+                    );
+                }
+            );
+
+            // Si connexion KO
+            cnn.addEventListener(
+                'error',
+                function () {
+                    alert('Erreur lors de la connexion !');
+                }
+            );
+        } else {
+            alert('IndexDB non supporté !');
         }
     }
 );
